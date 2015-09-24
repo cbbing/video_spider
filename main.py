2a1986e863d1268c6b0878f88d0777744ddc081a
cbbing <cbbing@163.com>
2015-09-24 15:19:38 +0800

    deployed

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import sys
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
+import time
+import shutil
 import pandas as pd
 from video_youku import YoukuVideo
 from video_tudou import TudouVideo
@@ -12,12 +14,30 @@ from video_tudou import TudouVideo
 
 if __name__ == "__main__":
 
-    data = pd.read_excel('keys.xlsx', 'Sheet3', index_col=None, na_values=['NA'])
+    data = pd.read_excel('C:\Users\Administrator\Desktop\Data\keys.xlsx', 'Sheet1', index_col=None, na_values=['NA'])
     print data
 
-    video = YoukuVideo()
-    video.run(data['key'].get_values())
+    t = raw_input("请确认以上关键字(输入 yes 继续,  其它键退出): ".encode('gbk'))
 
-    video = TudouVideo()
-    video.run(data['key'].get_values())
+    t = t.strip().lower()
+
+    if ( t == 'yes'):
+
+        keys = data['key'].get_values()
+
+        video = YoukuVideo()
+        video.filePath = 'C:/Users/Administrator/Desktop/Data/Result/youku_video.xlsx'
+        video.run(keys)
+
+        video = TudouVideo()
+        video.filePath = 'C:/Users/Administrator/Desktop/Data/Result/tudou_video.xlsx'
+        video.run(keys)
+
+        #复制目录
+        #shutil.copytree(r'C:\Code\video_spider\data', r'C:\Users\Administrator\Desktop\Data')
+        #shutil.copyfile(video.filePath,'C:\Users\Administrator\Desktop\Data\Result' )
+
+    else:
+        print '>>>  exit  >>>'
+        time.sleep(1)
 
